21ec489ce03208484f03cf10f94e1cc95ac9d266
reenadangi <reena.dangi@gmail.com>
2020-03-24 14:16:49 -0500

    selection sort

--- python_stack/Algos/sort_bubble.py
+++ python_stack/Algos/sort_bubble.py
@@ -1,9 +1,12 @@
-arr=[1,3,6,7,9,2]
+arr=[5,2,3,1]
 def bubble_sort(arr):
+    count=0
     for i in range(len(arr)):
+        count=count+1
         for j in range(i+1,len(arr),1):
+            count=count+1
             if arr[i]>arr[j]:
                 arr[i],arr[j]=arr[j],arr[i]
-       
+    print(count)
 bubble_sort(arr)
 print(arr)
---
+++ python_stack/Algos/sort_selection.py
@@ -0,0 +1,15 @@
+# Selection Sort
+
+# Find the minimum element in arr[0...4]
+#  and place it at beginning
+# 11 25 12 22 64
+my_arr = [5,2,3,1] 
+def sort(nums):
+    for i in range (len(nums)):
+        min_index=i
+        for j in range(i+1,len(nums)):
+            if nums[min_index] > nums[j]:
+                min_index=j
+        nums[i],nums[min_index]=nums[min_index],nums[i]
+sort(my_arr)
+print(my_arr)
