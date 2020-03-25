023d7796e289e97843e7ee0162192ce69d439181
reenadangi <reena.dangi@gmail.com>
2020-03-25 08:20:47 -0500

    MaxMinAvg

---
+++ python_stack/Algos/minMaxAvg.py
@@ -0,0 +1,15 @@
+def minMaxAvg(my_lst):
+    min=my_lst[0]
+    max=my_lst[0]
+    total=0
+
+    for i in my_lst:
+        if min>i:
+            min=i
+        if max<i:
+            max=i
+        total+=i
+    avg=total/len(my_lst)
+    print(f"Min {min}, Max {max}, Avg {avg}")
+minMaxAvg([2,3,4,5,6,7,1])
+        
