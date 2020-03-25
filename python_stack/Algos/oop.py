7231b969cfc1215162e0379ee2c9c7cae25949c5
reenadangi <reena.dangi@gmail.com>
2020-03-25 08:58:31 -0500

    Bank Account

--- python_stack/Algos/oop.py
+++ python_stack/Algos/oop.py
@@ -1,25 +1,37 @@
 class user:
    
     def __init__(self,name,email):
+
         self.name=name
         self.email=email
         self.accountBalance=100
+        self.interest_rate=0.05
     def display(self):
         print(f"Hi I am {self.name} Amount {self.accountBalance}")
         return self
-    def make_withdrawal(self,amount):
+    def deposite(self,amount):
+        self.accountBalance+=amount
+        return self
+    def withdrawal(self,amount):
         self.accountBalance-=amount
         return self
     def transfer_money(self,other_account,amount):
         self.accountBalance-=amount
         other_account.accountBalance+=amount
         return self
+    def yield_interest(self):
+        self.accountBalance+=self.accountBalance*self.interest_rate
+        return self
+        
         
 user1=user("Reena","r@go.com")
-user1.display().make_withdrawal(50).display()
-user2=user("Tom","t@go.com")
-user2.transfer_money(user1,10).display()
+user1.deposite(300)
+user1.display().withdrawal(50).display()
 
+user2=user("Tom","t@go.com")
+user1.transfer_money(user2,10).display()
+user1.yield_interest().display()
+user2.display()
 
 
 
