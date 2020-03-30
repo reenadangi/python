c8842d21b2b9ddcacaf8a20596a1591b1d3f696e
reenadangi <reena.dangi@gmail.com>
2020-03-30 16:28:31 -0500

    OOP inheritanceII

---
+++ python_stack/Algos/OOP/inheritance.py
@@ -0,0 +1,13 @@
+class BankAccount:
+    def __init__(self,int_rate,balance=0):
+        self.int_rate=int_rate
+        self.balance=balance
+    
+class retirementAccount(BankAccount):
+        def __init__(self,int_rate,is_routh,balance=0):
+               super(int_rate,balance)
+               self.is_roth=is_roth
+
+acc=retirementAccount(1.04,True,3000)
+
+      
