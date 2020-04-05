9c35654d623b1f7799ecfe09ce892b6f7c8a7fce
reenadangi <reena.dangi@gmail.com>
2020-04-05 12:16:13 -0500

    litings updated

--- estate_proj/estates/listings/views.py
+++ estate_proj/estates/listings/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
+from django.core.paginator import EmptyPage,PageNotAnInteger,Paginator
 from .models import Listing
+
 # Create your views here.
 def index(request):
-    listings=Listing.objects.all()
+    # order by list_date(latest show first)
+    listings=Listing.objects.order_by('-list_date').filter(is_published=True)
+    paginator=Paginator(listings,3)
+    page=request.GET.get('page')
+    page_listings=paginator.get_page(page)
     context={
-        'listings':listings
+        'listings': page_listings
     }
     return render(request,'listings/listings.html',context)
 
-def  listings(request):
-    return render(request,'listings/listings.html')
+def  listing(request,listing_id):
+    return render(request,'listings/listing.html')
 
 def search(request):
     return render(request,'listings/search.html')
