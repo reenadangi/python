3694b7a591bcf72aa06afd26380e04b1940ca254
reenadangi <reena.dangi@gmail.com>
2020-04-06 08:54:14 -0500

    listing updated

--- estate_proj/estates/listings/views.py
+++ estate_proj/estates/listings/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,get_object_or_404
 from django.core.paginator import EmptyPage,PageNotAnInteger,Paginator
 from .models import Listing
+from listings.choices import price_choices,bedroom_choices,state_choices
+
 
 # Create your views here.
 def index(request):
@@ -22,4 +24,11 @@ def  listing(request,listing_id):
     return render(request,'listings/listing.html',context)
 
 def search(request):
-    return render(request,'listings/search.html')
+    # listing=get_object_or_404(Listing,pk=listing_id)     
+    context={
+        #   'listing':listing,
+          'state_choices':state_choices,
+          'bedroom_choices':bedroom_choices,
+          'price_choices':price_choices,
+     }
+    return render(request,'listings/search.html',context)
