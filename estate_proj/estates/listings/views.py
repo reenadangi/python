0df0edce12e87dcb2522edba852ff396433331b0
reenadangi <reena.dangi@gmail.com>
2020-04-06 15:10:22 -0500

    Estates search updated

--- estate_proj/estates/listings/views.py
+++ estate_proj/estates/listings/views.py
@@ -27,10 +27,32 @@ def search(request):
     # listing=get_object_or_404(Listing,pk=listing_id)     
     queryset_list=Listing.objects.order_by('-list_date')
     #Keywords
+    if 'city' in request.GET:
+        city=request.GET['city']
+        if city:
+            queryset_list=queryset_list.filter(city__iexact=city)
+    #city
     if 'keywords' in request.GET:
         keywords=request.GET['keywords']
         if keywords:
             queryset_list=queryset_list.filter(description__icontains=keywords)
+    #state
+    if 'state' in request.GET:
+        state=request.GET['state']
+        if state:
+            queryset_list=queryset_list.filter(state__iexact=state)
+    #bedroom
+    if 'bedrooms' in request.GET:
+        bedrooms=request.GET['bedrooms']
+        if bedrooms:
+            queryset_list=queryset_list.filter(bedrooms__lte=bedrooms)
+    #price
+    if 'price' in request.GET:
+        price=request.GET['price']
+        if price:
+            queryset_list=queryset_list.filter(price__lte=price)
+    
+
     context={
         #   'listing':listing,
           'state_choices':state_choices,
