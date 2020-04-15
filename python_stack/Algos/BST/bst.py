1f99e3ff4d5f02faf0e3115f6668767756f61d59
reenadangi <reena.dangi@gmail.com>
2020-04-15 15:28:32 -0500

    Algos BST Height

--- python_stack/Algos/BST/bst.py
+++ python_stack/Algos/BST/bst.py
@@ -1,55 +1,61 @@
 class Node:
     def __init__(self,value=None):
-       self.value=value
-       self.left=None
-       self.right=None
+        self.value=value
+        self.left=None
+        self.right=None
 class BST:
     def __init__(self):
         self.root=None
     def insert(self,value):
+        # add a new node
         if self.root:
-            # call a dunder method with same signature
-            self._insert(value,self.root)
+            self._insert(self.root,value)
         else:
             self.root=Node(value)
-    def _insert(self,value,cur_node):
-        # Where to insert left/right
-        if value<cur_node.value:
-            # Insert to left
-            # check if there is any left
+    def _insert(self,cur_node,value):
+        # check where to insert left/right
+        if cur_node.value>value:
+            # insert at left side
             if cur_node.left==None:
-                # insert here
                 cur_node.left=Node(value)
-                return
-            self._insert(value,cur_node.left)
+            else:
+                self._insert(cur_node.left,value)
+
         else:
-            # insert to right
-            # check if there is right
+            # insert at right side
             if cur_node.right==None:
-                # insert here
                 cur_node.right=Node(value)
-                return
-            self._insert(value,cur_node.right)
-            
+            else:
+                self._insert(cur_node.right,value)
     def print_tree(self):
         if self.root:
             self._print_tree(self.root)
-    
     def _print_tree(self,cur_node):
-        # inorder travesal
-        if cur_node!=None:
+        if cur_node:
             self._print_tree(cur_node.left)
             print(cur_node.value)
             self._print_tree(cur_node.right)
+    def height(self):
+        if self.root:
+            return self._height(self.root)
+    def _height(self,cur_node):
+        if cur_node==None:
+            return -1
+        else:
+            left_height=self._height(cur_node.left)
+            right_height=self._height(cur_node.right)
+            return max(left_height,right_height)+1
 
 bst=BST()
 bst.insert(12)
+bst.insert(13)
 bst.insert(10)
+bst.insert(14)
+bst.insert(16)
 bst.insert(15)
+bst.insert(3)
+
 bst.print_tree()
+print(bst.height())      
+       
 
-
-
-
-
-  
