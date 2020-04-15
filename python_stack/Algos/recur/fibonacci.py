a949354b02ba4205a1111b890190ecc54d88631a
reenadangi <reena.dangi@gmail.com>
2020-04-14 21:03:33 -0500

    Algos fibonacci

--- python_stack/Algos/recur/fibonacci.py
+++ python_stack/Algos/recur/fibonacci.py
@@ -3,12 +3,11 @@
 # print nth fibonacci number
 def fibonacci(n):
     if n<0:
-        print("Invalid")
+        return False
     elif n==0 or n==1:
         return n
-    elif n==1:
-        return 1
     else:
         return fibonacci(n-1)+fibonacci(n-2)
+
     
-print(fibonacci(1))
+print(fibonacci(4))
