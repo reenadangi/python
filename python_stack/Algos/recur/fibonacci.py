a82a56c4811653203957b1691e128bc18be0d2af
reenadangi <reena.dangi@gmail.com>
2020-04-14 20:59:10 -0500

    Algos fib

---
+++ python_stack/Algos/recur/fibonacci.py
@@ -0,0 +1,14 @@
+# he Fibonacci numbers are the numbers in the following integer sequence.
+# 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ……..
+# print nth fibonacci number
+def fibonacci(n):
+    if n<0:
+        print("Invalid")
+    elif n==0 or n==1:
+        return n
+    elif n==1:
+        return 1
+    else:
+        return fibonacci(n-1)+fibonacci(n-2)
+    
+print(fibonacci(1))
