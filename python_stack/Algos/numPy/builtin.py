9a697536e9eabcb86d75f71d0805ce503cb939e5
reenadangi <reena.dangi@gmail.com>
2020-04-28 16:32:19 -0500

    Algos numpy built ins

--- python_stack/Algos/numPy/builtin.py
+++ python_stack/Algos/numPy/builtin.py
@@ -9,3 +9,17 @@ w=np.eye(4)
 print(w)
 y=np.diag([10,20,30,50])
 print(y)
+
+# arange(start,stop,intervel)
+x=np.arange(1,10)
+# 1-9
+print(x)
+x=np.arange(4,10)
+print(x)
+x=np.arange(0,100,10)
+# 10 20 30 40 50 60 70 80 90
+print(x)
+
+# linspace(start,stop,end)
+x=np.linspace(0,10,100)
+print(x)
